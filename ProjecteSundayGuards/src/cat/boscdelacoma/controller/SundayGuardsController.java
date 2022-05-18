65f5e7fbdeebfd81acd33f371161db48344231eb
victorfb98 <v.flores5435@gmail.com>
2022-05-18 10:05:17 +0200

    Modificacions i alguns mètodes de Guardia

--- ProjecteSundayGuards/src/cat/boscdelacoma/controller/SundayGuardsController.java
+++ ProjecteSundayGuards/src/cat/boscdelacoma/controller/SundayGuardsController.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package cat.boscdelacoma.controller;
 
@@ -9,26 +9,35 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
+ * FXML Controller class
  *
  * @author Victo
  */
 public class SundayGuardsController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private TextField inputDni;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TextField inputPassword;
+    @FXML
+    private Button btnAccedir;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnAccioAccedir(ActionEvent event) {
+    }
+
     
 }
