6546720ac40dc708ee1d28f3208d9202c8a0bb87
victorf98 <vflores111@boscdelacoma.cat>
2022-05-30 10:12:08 +0200

    Canvis

--- ProjecteSundayGuards/src/cat/boscdelacoma/controller/SundayGuardsController.java
+++ ProjecteSundayGuards/src/cat/boscdelacoma/controller/SundayGuardsController.java
@@ -8,13 +8,12 @@ import cat.boscdelacoma.model.persistence.dao.impl.jdbc.mysql.JDBCTreballadorDAO
 import cat.boscdelacoma.model.business.entities.Treballador;
 import cat.boscdelacoma.model.persistence.exceptions.DAOException;
 import cat.boscdelacoma.view.console.Menu;
-import cat.boscdelacoma.view.console.SHA512;
-import cat.boscdelacoma.view.console.testos;
 import javafx.application.Platform;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 import java.util.ResourceBundle;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -45,84 +44,81 @@ public class SundayGuardsController implements Initializable {
      * Initializes the controller class.
      */
     private Stage stage;
-   
-    
-    public void setStage (Stage stage){
+
+    public void setStage(Stage stage) {
         this.stage = stage;
-        
-   }
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-   
-    }    
+
+    }
 
     @FXML
-    private void btnAccioAccedir(ActionEvent event) {
-        
-        if(espaisPlens(inputDni,inputContrasenya)){
-            JDBCTreballadorDAO treballador=new JDBCTreballadorDAO();
-                    
-            String dni= inputDni.getText();
-            String contrasenya= inputContrasenya.getText();
-            inputDni=limitText(inputDni,20);
+    private void btnAccioAccedir(ActionEvent event) throws SQLException {
+
+        if (espaisPlens(inputDni, inputContrasenya)) {
+            JDBCTreballadorDAO treballador = new JDBCTreballadorDAO();
+
+            String dni = inputDni.getText();
+            String contrasenya = inputContrasenya.getText();
+            inputDni = limitText(inputDni, 20);
             /*/contrasenya= SHA512(contrasenya, "Perrito");/*/
 
-                try{
-                    Treballador treb=treb.get(Long.parseLong(dni), contrasenya);
-                                        //s'ha d'implementar mètode per DNI i contrasenya//
-                }catch(DAOException ex){
-                    Alert alert= new Alert(Alert.AlertType.ERROR);
-                    System.out.println("Error al obtenir treballador per mètode DNI i contrasenya"+ ex.getMessage());
-                    alert.setHeaderText("Error de la base de dades");
-                    alert.show();
-                }      
-                
-                if (treballador==null){
-                    
-                    Alert alert= new Alert(Alert.AlertType.ERROR);
-                    
+            try {
+                Treballador treb = treballador.getTreballadorLogin(dni, contrasenya);
+                //s'ha d'implementar mètode per DNI i contrasenya//
+
+                if (treb == null) {
+
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+
                     alert.setHeaderText("Dades incorrectes");
                     alert.show();
-                }
-                else{
+                } else {
                     //enviar treballador per paràmetre al main//
-                    
-                    testos.main(treb);
-                    
+
+                    Menu m = new Menu();
+                    m.menuInicial(treb);
+
                     stage.hide();
-                    
-                    
+
                 }
-                
-                
-                
+            } catch (DAOException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                System.out.println("Error al obtenir treballador per mètode DNI i contrasenya" + ex.getMessage());
+                alert.setHeaderText("Error de la base de dades");
+                alert.show();
             }
-            
+
         }
 
-    private boolean espaisPlens(TextField inputDni, TextField contrasenya){
-        
-        if(inputDni.getText().equals("")||inputContrasenya.getText().equals("")){
-        
-            Alert alert= new Alert(Alert.AlertType.ERROR);
-            
+    }
+
+    private boolean espaisPlens(TextField inputDni, TextField contrasenya) {
+
+        if (inputDni.getText().equals("") || inputContrasenya.getText().equals("")) {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+
             alert.setHeaderText("Introdueix totes les dades corresponents!");
-            
+
             alert.show();
             return false;
-  
-    }
+
+        }
         return true;
 
     }
-    
-    public static TextField limitText(TextField textField, int length){
-        
+
+    public static TextField limitText(TextField textField, int length) {
+
         String string = textField.getText();
-        
-        if(string.length()>length){
-            textField.setText(string.substring(0,length));
-            
+
+        if (string.length() > length) {
+            textField.setText(string.substring(0, length));
+
         }
         return textField;
     }
@@ -130,12 +126,6 @@ public class SundayGuardsController implements Initializable {
     @FXML
     private void btnAccioCancelar(ActionEvent event) {
         stage.hide();
-        }
+    }
 
-   
 }
-    
-
-
-    
-
--- ProjecteSundayGuards/src/cat/boscdelacoma/view/console/Menu.java
+++ ProjecteSundayGuards/src/cat/boscdelacoma/view/console/Menu.java
@@ -45,7 +45,7 @@ public class Menu {
 
     static Scanner entrada;
 
-    public static void menuInicial(Treballador treballador) throws DAOException, SQLException {
+    public void menuInicial(Treballador treballador) throws DAOException, SQLException {
         /*Creem un objecte Treballador d'exemple amb el rol d'Administrador, si
         el treballador té rol d'Admin (coincideix) accedirà al menú 
         d'administració, en cas contrari anirà al menú d'usuari*/
